23e275b85358bf6b4dfb1c5bc6a470b9844d85d9
Mayank Tanwani <tanwani.mayank@gmail.com>
2018-02-20 04:21:34 +0530

    Updated Permissions

--- app/src/main/java/com/example/mayank/savecontacts/MainActivity.java
+++ app/src/main/java/com/example/mayank/savecontacts/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mayank.savecontacts;
 import android.Manifest;
 import android.content.pm.PackageManager;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -10,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -21,6 +23,8 @@ import io.reactivex.schedulers.Schedulers;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    public View rootView;
+    final int PERMISSION_REQUEST_CODE = 23;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,22 +33,40 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-    public void saveContacts(final View view) {
+    public void setupPermission() {
         if ((ContextCompat.checkSelfPermission(MainActivity.this,
                 Manifest.permission.READ_CONTACTS)
                 != PackageManager.PERMISSION_GRANTED)
                 || (ContextCompat.checkSelfPermission(MainActivity.this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                    Manifest.permission.READ_CONTACTS) && ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            } else {
-                ActivityCompat.requestPermissions(MainActivity.this,
-                        new String[]{Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                        23);
-            }
+            String[] permissionNeeded = new String[]{Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            requestPermissions(permissionNeeded, PERMISSION_REQUEST_CODE);
+        } else {
+                performOps(rootView);
         }
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode)
+        {
+            case PERMISSION_REQUEST_CODE : {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    performOps(rootView);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Permission not granted", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+
+        }
+    }
+
+    public void performOps(View view) {
         io.reactivex.Observable.fromCallable(() -> {
             BackupContacts b = new BackupContacts(MainActivity.this);
             b.saveAllContacts();
@@ -56,5 +78,11 @@ public class MainActivity extends AppCompatActivity {
                 .subscribe((result) -> {
                     Snackbar.make(view.getRootView(),"All Contacts Backed Up",Snackbar.LENGTH_SHORT).show();
                 });
+    }
+
+
+    public void saveContacts(final View view) {
+        rootView = view;
+        setupPermission();
     }
 }
